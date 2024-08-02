aac34259f13f85b90635878b28f56b544a1d1ffb
Sophia <sophiaajibola16@gmail.com>
2024-08-02 14:11:16 +0100

    Updated Update Controller

--- Controllers/toDoController.js
+++ Controllers/toDoController.js
@@ -11,7 +11,7 @@ const create = AsyncHandler(async(req,res, next)=>{
             throw new Error("Invalid Input");
         }
         const user = await sql`INSERT into todo
-        (username, taskname, description)
+        (username, taskname, description )
         VALUES
         (${username}, ${taskname}, ${description})
         returning id, username, taskname, description, status, taskdate`;
@@ -26,32 +26,48 @@ const create = AsyncHandler(async(req,res, next)=>{
 
 }) 
 
-// //update controller
-// const update = AsyncHandler(async(req, res, next)=>{
-//     try{
-//         const {id} = req.body;
-//         if(id){     
-//             const fetchUser =
-//             await sql `SELECT * from todo WHERE id = ${id}`;
-//             const user = fetchUser[0]; //this returns first result of fetchUser function
-//             if(user){
-//                 await sql `UPDATE todo SET username = ${username}, taskname = ${taskname}, description = ${description}
-//                 WHERE id = ${user.id}`;
-//                 return res.status(200).json({
-//                     message: "task update successful"
-//                 });
-//             }
-//         } else{
-//             return res.status(404).json({
-//                 message: "update failed"
-//             })
-//         }
+//update controller
+const update = AsyncHandler(async(req, res, next)=>{
+    try{
+        const {id, username, taskname, description, status, taskdate } = req.body;
+        console.log("fine too");
+        if(!id){     
+            res.status(400);
+            throw new Error("id required");
+        } 
 
-//     }catch(error){
-//         next(error);
-//     }
-// })
+        const fetchUser =
+            await sql `SELECT * from todo WHERE id = ${id}`;
+            if(fetchUser.length == 0){
+                return res.status(404).json({
+                    message: "User not found"
+                });
+            }
+            const user = fetchUser[0]; //this returns first result of fetchUser function
+
+    
+
+            if(user.id == id){
+                await sql `UPDATE todo SET username = ${username}, taskname = ${taskname}, description = ${description}
+                WHERE id = ${user.id}`;
+                console.log("perfect here");
+                return res.status(200).json({
+                    message: "task update successful"
+                });
+                
+            } else{
+            return res.status(404).json({
+                message: "update failed"
+            })
+        }
+        
+
+    }catch(error){
+        next(error);
+    }
+})
 
 
 
-export {create};
+
+export {create, update};
--- Routes/todoRouter.js
+++ Routes/todoRouter.js
@@ -1,7 +1,11 @@
 //authRouter.route("/users").post(register)
 import express from "express";
-import {create} from "../Controllers/toDoController.js";
+import {create, update} from "../Controllers/toDoController.js";
+
 
 export const todoRouter = express.Router()
 todoRouter.route("/user").post(create)
+todoRouter.route("/user/update").post(update)
+
+
 
